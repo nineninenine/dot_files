155534444a32a1603409b97da11ca221be6303ed
nineninenine <ehobart@gmail.com>
2023-03-12 19:31:17 -0500

    updated to zsh style powershell autocomplete

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -4,8 +4,18 @@
 #disable truncated output https://mcsaguru.com/how-to-fix-truncated-powershell-output/
 $FormatEnumerationLimit=-1
 
-#bash style tab complete
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
+# zsh style tabcomplete - Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 #disable horrible bell sound to keep my ears from bleeding ;)
 Set-PSReadlineOption -BellStyle None
+
+
+
+
+
